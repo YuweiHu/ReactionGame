af1effb1b09cb5e5d5eb00181fa9a4a9e551c3e4
Yo wei Hu <j821220j821220@gmail.com>
2019-06-24 22:11:08 +0800

    Upload File

---
+++ app.js
@@ -0,0 +1,28 @@
+var createdTime;
+var clickedTime;
+var reactTime;
+
+function makeBox() {
+    // //2000毫秒內的隨機秒數
+    // var time = Math.random() * 2000;
+    setTimeout(function() {
+        //顯示方塊
+        document.getElementById('box').style.display = 'block';
+        createdTime = Date.now();
+        //讓方塊出現在隨機位置
+        randPositionTop = Math.floor(120 + Math.random() * (window.innerHeight - 280));
+        randPositionLeft = Math.floor(Math.random() * (window.innerWidth - 140));
+        document.getElementById('box').style.top = randPositionTop + 'px';
+        document.getElementById('box').style.left = randPositionLeft + 'px';
+    }, 100);
+}
+
+document.getElementById('box').onclick = function() {
+    this.style.display = 'none';
+    clickedTime = Date.now();
+    reactTime = (clickedTime - createdTime) / 1000;
+    makeBox();
+    document.getElementById('time').innerHTML = reactTime;
+}
+
+makeBox();
